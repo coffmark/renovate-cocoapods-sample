a28a7377face7a6ba4daf833405fd6069aed92d0
coffmark <come1234560@gmail.com>
2022-11-13 12:45:01 +0900

    initial commit

---
+++ renovate-cocoapods-sample/renovate-cocoapods-sample/Sample.swift
@@ -0,0 +1,9 @@
+//
+//  Sample.swift
+//  renovate-cocoapods-sample
+//  
+//  Created by coffmark on 2022/11/13
+//  
+//
+
+import Foundation
---
+++ renovate-cocoapods-sample/renovate-cocoapods-sample/renovate_cocoapods_sampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  renovate_cocoapods_sampleApp.swift
+//  renovate-cocoapods-sample
+//  
+//  Created by coffmark on 2022/11/13
+//  
+//
+
+import SwiftUI
+
+@main
+struct renovate_cocoapods_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
